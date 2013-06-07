f8b158a7963f95d1781fe1a5354c78395fc4140e
Shlomi Fish <shlomif@shlomifish.org>
2013-06-07 15:54:59 +0300

    Solved 6x6 Very Hard Board No. 2.

--- ruby/test/deduction.rb
+++ ruby/test/deduction.rb
@@ -480,6 +480,30 @@ EOF
     return Binary_Puzzle_Solver.gen_board_from_string_v1(input_str)
 end
 
+def get_6x6_very_hard_board_2__initial()
+    input_str = <<'EOF'
+| 0    |
+|      |
+|1     |
+| 0  0 |
+|1  0  |
+| 0   0|
+EOF
+    return Binary_Puzzle_Solver.gen_board_from_string_v1(input_str)
+end
+
+def get_6x6_very_hard_board_2__final()
+    input_str = <<'EOF'
+|001011|
+|010011|
+|110100|
+|001101|
+|110010|
+|101100|
+EOF
+    return Binary_Puzzle_Solver.gen_board_from_string_v1(input_str)
+end
+
 describe "construct_board" do
     it "6*6 Easy board No. 1 should" do
 
@@ -1073,6 +1097,30 @@ describe "rudimentary_deduction" do
         ).should == ZERO
 
         final_board = get_6x6_very_hard_board_1__final()
+
+        compare_boards(board, final_board)
+    end
+
+    it "Solving 6*6 Very Hard board No. 2 should" do
+
+        board = get_6x6_very_hard_board_2__initial()
+
+        board.add_to_iters_quota(1_000_000_000);
+
+        board.try_to_solve_using(
+            :methods => [
+                :check_and_handle_sequences_in_row,
+                :check_and_handle_known_unknown_sameknown_in_row,
+                :check_and_handle_cells_of_one_value_in_row_were_all_found,
+                :check_exceeded_numbers_while_accounting_for_two_unknown_gaps,
+                :check_try_placing_last_of_certain_digit_in_row,
+                :check_try_placing_last_of_certain_digit_in_row_to_avoid_dups,
+            ]
+        );
+
+        final_board = get_6x6_very_hard_board_2__final()
+
+        # binding.pry
 
         compare_boards(board, final_board)
     end
